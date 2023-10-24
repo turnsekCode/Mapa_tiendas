7074612ce18cd1cfebdc72688fdffa67e774c83f
dariusset <darsagan@gmail.com>
2023-10-24 15:34:49 +0200

    Tiendas y Ciudades: Cambio texto botones LLAMA GRATIS

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/compro-oro-madrid":["static\u002Fchunks\u002Fpages\u002Fcompro-oro-madrid.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fcompro-oro-madrid"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
